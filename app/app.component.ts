a6c582ee04c3e5bb9dd3ba52db66809cd655fa25
Pablo Ledesma <pabloledes83@gmail.com>
2016-07-22 23:46:32 -0500

    inicio del primer componente | se creó el modelo Place

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'super-lp',
+	templateUrl: 'app/app.component.html'
+})
+
+export class AppComponent {
+	title = 'Super Landing Page';
+	subTitle = 'This landing page is build whit Angular2!';
+}
---
+++ app/mock-places.ts
@@ -0,0 +1,16 @@
+import { Place } from './model/Place';
+
+export var origin: Place[] = [
+	{"name":"Boston, US", iata:"BOS"},
+    {"name":"Chicago, US", iata:"ORD"},
+    {"name":"Fort Lauderdale, US", iata:"FLL"},
+    {"name":"Las Vegas, US", iata:"LAS"},
+    {"name":"Los Angeles, US", iata:"LAX"},
+    {"name":"Miami, US", iata:"MIA"},
+    {"name":"New York, US", iata:"JFK"},
+{"name":"New Orleans, US", iata:"MSY"},
+{"name":"San Francisco, US", iata:"SFO"},
+    {"name":"Orlando, US", iata:"MCO"},
+    {"name":"Tampa, US", iata:"TPA"},
+    {"name":"Washington Dulles, US", iata:"IAD"}    
+];
